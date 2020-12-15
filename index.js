3ebb947cba47ed5d6e11e4e608a5479988b23646
Jonathan Hill <hill_jonathan@bah.com>
2020-12-15 14:59:09 -0500

    update to a response

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const puppeteer = require('puppeteer');
-const fs = require('fs'); 
+const fs = require('fs');
+const path = require('path');
 
 exports.handler = async (event, context, callback) => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://usaspending.gov', { waitUntil: "networkidle2" });
   const html = await page.content();
-  console.log(' HTML : ', html);
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: html,
+  };
   await browser.close();
-  return callback(null, html);
+  return callback(null, response);
 };
